28319b2cd8d7f4d7709e629d4b1393d5a1c79f4f
bhattarainemi <bhattarainemi7@gmail.com>
2026-08-22 18:39:27 +0545

    update

--- shape2graph/shape2graph.py
+++ shape2graph/shape2graph.py
@@ -6,5 +6,6 @@ import ipyleaflet
 class Map(ipyleaflet.Map):
     """A map class that extends ipyleaflet.Map."""
 
-    def __init__(self, center=[20, 0], zoom=2, **kwargs):
+    def __init__(self, center=[20, 0], zoom=2, height="600px", **kwargs):
         super().__init__(center=center, zoom=zoom, **kwargs)
+        self.layout.height = height
